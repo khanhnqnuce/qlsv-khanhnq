f229e1093bd22c98d556a8b0e38f047481123655
khanhnqnuce <khanh060992@gmail.com>
2015-09-05 09:39:24 +0700

    update

---
+++ FDI/jQuery Function/jQuery Function/Controllers/DemoUIController.cs
@@ -0,0 +1,16 @@
+﻿using System.Web.Mvc;
+
+namespace jQuery_Function.Controllers
+{
+    public class DemoUIController : Controller
+    {
+        //
+        // GET: /DemoUI/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
